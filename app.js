5dec3a53c8a478de4ca6724fe2687b504291d31f
Anish Kumar <77631750+anishkumar127@users.noreply.github.com>
2022-12-06 10:46:26 +0000

    model and schema created

--- app.js
+++ app.js
@@ -1,12 +1,30 @@
-require('dotenv').config();
-const express = require('express');
+require("dotenv").config();
+const express = require("express");
 const app = express();
 
+// connect database calling
+require("./database/database").connect();
+
+// middleware
+app.use(express.json());
 
 // home route
-app.get("/",(req,res)=>{
-    res.send("hello is it running see it now");
-})
+app.get("/", (req, res) => {
+  res.send("hello is it running see it now");
+});
 
+app.post("/register", async (req, res) => {
+  try {
+    // steps
+    // 1. get all data from body
+    // 2. all the data should exists
+    // 3. check if user already exists
+    // 4. encrypt the password
+    // 5. save the user in Database
+    // 6. generate a token for user and send it.
+  } catch (err) {
+    console.log(err);
+  }
+});
 
-module.exports = app
+module.exports = app;
