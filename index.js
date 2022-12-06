de6912501e976ca33fc4c154bb972c8f9abc4509
Anish Kumar <77631750+anishkumar127@users.noreply.github.com>
2022-12-06 10:03:26 +0000

    server created done

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const app = require('./app');
-const PORT = process.env || 8000;
+const {PORT} = process.env || 8000;
 
 app.listen(PORT,()=>{
     console.log(`Server is running  at port : ${PORT}`);
